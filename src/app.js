67432ebfb993a036c9a7989d3a8fe20b24f8e40b
Lili Fuentes <lile7886@gmail.com>
2021-01-03 14:54:49 -0500

    added client origin for cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const foldersRouter = require('./folders/folders-router')
@@ -16,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({ origin: CLIENT_ORIGIN }))
 app.use(express.json())
 
 app.use('/folders', foldersRouter)
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lili@localhost/noteful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://lili@localhost/noteful-test"
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://ndrhhrxyiuaitj:ca4337b33d74012ef4647db76b0fd544648ba4d5d77331a4e5669bbdbd3749ca@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d6ooi7bgr7v119',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://lili@localhost/noteful-test",
+    CLIENT_ORIGIN: 'https://noteful-client-ashen-nine.vercel.app/'
 }
